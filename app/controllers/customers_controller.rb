9810cc8e2c30dadea5bc7e9a7d5378c2c25d4d52
Chelsea Bahsler <chelseabahsler@gmail.com>
2023-11-13 15:23:55 -0600

    finished part 2

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -6,4 +6,12 @@ class CustomersController < ApplicationController
   def show
     @cust = Customer.find(params[:id])
   end
+
+  def alphabetized
+    @customers = Customer.order(:full_name)
+  end
+
+  def missing_email
+    @customers = Customer.where("email == ''")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   ActiveAdmin.routes(self)
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
   root "customers#index"
-  resources :customers, only: %i[index show]
+  get "alphabetized", to: "customers#alphabetized"
+  get "missing_email", to: "customers#missing_email"
 end
